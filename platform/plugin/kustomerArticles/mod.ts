b8b5b7add24a6067eff59ff457251689214f5367
Lily <liwang@yext.com>
2023-02-22 18:05:55 -0500

    editing url function

--- platform/plugin/kustomerArticles/mod.ts
+++ platform/plugin/kustomerArticles/mod.ts
@@ -2,7 +2,14 @@
 declare const domain: string;
 
 //Appends the KB domain to the source and hash ID URL path to generate the full URL
-export function createKustomerUrl(urlPath:string) {
-    const url = `${domain}` + urlPath;
-    return url
+export function createArticleUrl(urlPath:string) {
+    if (domain.slice(-1) == "/") {
+        //if the user accidentally adds the "/" on the end of the domain url
+        const url = `${domain}` + urlPath;
+        return url
+    }
+    else {
+        const url = `${domain}` + `/${urlPath}`;
+        return url
+    }
 }
